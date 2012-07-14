ba49ef8d25d209398c50f7e2b8d32c4ddc83d0f8
Alexander Kuklev <akuklev@akuklev-ntb.local>
2012-07-14 13:31:39 +0200

    gen1 works somehow

--- scala/src/main/scala/strategies/Genetic1.scala
+++ scala/src/main/scala/strategies/Genetic1.scala
@@ -5,12 +5,12 @@ trait Genetic1 {
   self: emulator.Commands with emulator.Games with emulator.Items with emulator.Points with emulator.States with emulator.Worlds =>
 
   def genetic1(game: State): Commands = {
-    def initialSize = 100
-    def maxLength = game.w.h * game.w.w / 5
+    def initialSize = 50
+    def ncrossover = initialSize / 2
+    def maxLength = game.w.h * game.w.w
 
-    def seed = 42
-    def rng = new scala.util.Random(seed)
-    def nextRandom: Int = rng.nextInt
+    def seed = System.currentTimeMillis()
+    val rng = new scala.util.Random(seed)
     def pickRandom[T](xs: T*): T = xs.toSeq(rng.nextInt(xs.toSeq.length))
 
     type TaggedSeq = List[(Point, Command)]
@@ -25,39 +25,57 @@ trait Genetic1 {
 
     def gen: Iterator[TaggedSeq] = {
       def genOne: TaggedSeq = {
-        var curPos = game.w.robot
-        var moves = TaggedSeq()
+        var points = collection.mutable.Set[Point]()
         var attempts = -1
+        var g = game
         def nextMove: (Point, Command) = {
           attempts += 1
-          if (attempts >= maxLength) (curPos, Abort)
+          if (attempts >= maxLength) (g.w.robot, Abort)
           else {
             val wannabe = pickRandom(Right, Left, Down, Up)
-            val failed = moves find (_._1 == (curPos + wannabe.dir)) isDefined;
-            if (failed) nextMove else { curPos = curPos + wannabe.dir; (curPos, wannabe) }
+            g match {
+              case ip: InProgress =>
+                val nextState = ip.step(wannabe)
+                val nextPos = nextState.w.robot
+                val failed = points(nextPos)
+                if (failed) nextMove else {
+                  g = nextState
+                  points += nextPos
+                  (nextPos, wannabe)
+                }
+              case _ => (g.w.robot, Abort)
+            }
           }
         }
         val seq = Stream.continually(nextMove).takeWhile(x => x._2 != Abort).toList
-        seq :+ (Invalid, Abort)
+        seq :+ (seq.last._1, Abort)
       }
       Stream.continually(genOne).iterator
     }
 
     def evolve(p: Population): Population = {
-      val allHybrids = for (a <- p; b <- p if (a.map(_._1).toSet intersect b.map(_._1).toSet).nonEmpty) yield crossover(a, b)
-      val hybrids = allHybrids take p.size/5
-      ((p ++ hybrids) take 4*p.size/5) ++ mkPopulation(p.size/5)
+      val allHybrids = for (a <- p.view; b <- p.view if (a.map(_._1).toSet intersect b.map(_._1).toSet).nonEmpty) yield crossover(a, b)
+      val hybrids = allHybrids take ncrossover
+      ((p ++ hybrids) take (p.size - ncrossover)) ++ mkPopulation(ncrossover)
     }
 
     def crossover(s1: TaggedSeq, s2: TaggedSeq): TaggedSeq = {
-      val ixn = pickRandom(s1.map(_._1) intersect s2.map(_._1): _*)
-      val i1 = s1 indexOf ixn
-      val i2 = s2 indexOf ixn
+      val points = s1.map(_._1) intersect s2.map(_._1)
+      val ixn = pickRandom(points: _*)
+
+      val i1 = s1 indexWhere (_._1 == ixn)
+      val i2 = s2 indexWhere (_._1 == ixn)
+      println(i1 + "-" + i2)
       (s1 take i1) ++ (s2 drop i2)
     }
 
     var p = mkPopulation()
-    for (i <- 0 to 100) p = evolve(p)
+    println(p map (_ map (_._2) mkString) mkString "\n")
+    for (i <- 0 to 20) {
+      p = evolve(p)
+      println(i)
+      println(p map (_ map (_._2) mkString) mkString "\n")
+    }
 
     val chosenOne = p.head
     val commands = chosenOne map (_._2)
