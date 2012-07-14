e98a81aadd619283b33ee7797b505e6bc2e5a79e
Alexander Temerev <temerev@miriamlaurel.com>
2012-07-14 21:50:20 +0200

    Algorithms removed from master

--- scala/src/main/scala/algorithms/graphs/MST.scala
+++
@@ -1,9 +0,0 @@
-package icfp.algorithms.graphs
-
-import icfp.algorithms.core._
-
-// source: http://code.google.com/p/scalgorithm
-
-trait MSTAlgo[A]{
-  def generateMST(graph:Graph[A]): Tree[Vertex[A]]
-}
