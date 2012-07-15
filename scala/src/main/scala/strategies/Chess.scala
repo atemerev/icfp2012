04c9fbbd1ff9fc740969aa27fe6255ac0308d5ba
Alexander Temerev <temerev@miriamlaurel.com>
2012-07-15 12:19:57 +0200

    we rock

--- scala/src/main/scala/strategies/Chess.scala
+++ scala/src/main/scala/strategies/Chess.scala
@@ -5,7 +5,7 @@ trait Chess {
 
   self: emulator.Commands with emulator.Games with emulator.Items with emulator.Points with emulator.States with emulator.Worlds =>
 
-  val MAX_LEVEL = 5
+  val MAX_LEVEL = 6
 
   def score(state: State): Int = {
     // limited path to nearest lambda
@@ -15,9 +15,9 @@ trait Chess {
     if (state.w.robot == Invalid) Int.MinValue else {
       val lambdas = state.collectedLambdas
       val open = if (state.w.liftIsOpen) 1 else 0
-      val distToLambda = state.w.remainingLambdaPositions.map(p => p.distanceTo(state.w.robot)).min
+      val distToLambda = if (state.w.liftIsOpen) 0 else state.w.remainingLambdaPositions.map(p => p.distanceTo(state.w.robot)).min
       val distToLift = state.w.robot.distanceTo(state.w.lift)
-      lambdas * 100 + open * 1000 + (2000 / distToLift) * open + distToLambda * 10
+      lambdas * 100 + open * 1000 + (2000 / distToLift) * open - distToLambda * 10
     }
   }
 
@@ -26,13 +26,14 @@ trait Chess {
   def step(ip: InProgress, c: Command) = cache.getOrElseUpdate((ip, c), ip.step(c))
 
   case class Node(parent: Node, command: Command, state: State, level: Int) {
-    lazy val children: List[Node] =
+    lazy val children: List[Node] = {
 //      if (level >= MAX_LEVEL) Nil else List(Up, Down, Left, Right, Abort, Wait).flatMap(c => state match {
-      if (level >= MAX_LEVEL) Nil else List(Up, Down, Left, Right, Wait).flatMap(c => state match {
+      val commands = List(Up, Down, Left, Right, Wait).filter(c => state.w(state.w.robot + c.dir).isPassable)
+      if (level > MAX_LEVEL) Nil else commands.flatMap(c => state match {
       case ip: InProgress => Some(Node(this, c, step(ip, c), level + 1))
         case _ => None
       })
-
+    }
     lazy val leaves: List[Node] = if (children.nonEmpty) children.flatMap(_.leaves) else List(this)
 
     lazy val commands: Commands = if (parent == null) Nil else parent.commands.toList :+ command
@@ -45,12 +46,18 @@ trait Chess {
     var g = game
     while (!g.terminal && g.commands.size <= g.w.w * g.w.h) {
       if (trace) println(g.w)
-      g = mkTree(game).leaves.maxBy(l => score(l.state)).state
+      val leaves = mkTree(g).leaves
+      if (trace) {
+        //leaves.foreach(l => println("%s: %s".format(l.commands.mkString, score(l.state))))
+      }
+      val maxBy = leaves.maxBy(l => score(l.state))
+      g = maxBy.state
     }
     val commands = g.commands
     if (trace) {
       println(g.w)
       println(commands mkString)
+      println(g.score)
     }
     commands
   }
