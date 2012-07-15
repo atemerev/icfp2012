e7c4d20aa66f1b8b89ca44d732e7f5d103b434b3
Alexander Temerev <temerev@miriamlaurel.com>
2012-07-15 12:42:40 +0200

    Distance to lambda (#1 from futamura)

--- scala/src/main/scala/strategies/Chess.scala
+++ scala/src/main/scala/strategies/Chess.scala
@@ -3,9 +3,11 @@ package strategies
 
 trait Chess {
 
-  self: emulator.Commands with emulator.Games with emulator.Items with emulator.Points with emulator.States with emulator.Worlds =>
+  self: emulator.Commands with emulator.Games with emulator.Items with emulator.Points with emulator.States with emulator.Worlds with Pathfinder =>
 
   val MAX_LEVEL = 6
+
+  var maps = Map[Point, DistanceMap]()
 
   def score(state: State): Int = {
     // limited path to nearest lambda
@@ -15,7 +17,10 @@ trait Chess {
     if (state.w.robot == Invalid) Int.MinValue else {
       val lambdas = state.collectedLambdas
       val open = if (state.w.liftIsOpen) 1 else 0
-      val distToLambda = if (state.w.liftIsOpen) 0 else state.w.remainingLambdaPositions.map(p => p.distanceTo(state.w.robot)).min
+//      val distToLambda = if (state.w.liftIsOpen) 0 else state.w.remainingLambdaPositions.map(p => p.distanceTo(state.w.robot)).min
+      val distToLambda = if (state.w.liftIsOpen) 0 else {
+        state.w.remainingLambdaPositions map (p => maps(p)(state.w.robot.x)(state.w.robot.y)) min
+      }
       val distToLift = state.w.robot.distanceTo(state.w.lift)
       lambdas * 100 + open * 1000 + (2000 / distToLift) * open - distToLambda * 10
     }
@@ -28,9 +33,9 @@ trait Chess {
   case class Node(parent: Node, command: Command, state: State, level: Int) {
     lazy val children: List[Node] = {
 //      if (level >= MAX_LEVEL) Nil else List(Up, Down, Left, Right, Abort, Wait).flatMap(c => state match {
-      val commands = List(Up, Down, Left, Right, Wait).filter(c => state.w(state.w.robot + c.dir).isPassable)
+      val commands = List(Up, Down, Left, Right, Wait)
       if (level > MAX_LEVEL) Nil else commands.flatMap(c => state match {
-      case ip: InProgress => Some(Node(this, c, step(ip, c), level + 1))
+        case ip: InProgress if step(ip, c).w.robot != state.w.robot => Some(Node(this, c, step(ip, c), level + 1))
         case _ => None
       })
     }
@@ -41,11 +46,11 @@ trait Chess {
 
   def mkTree(state: State) = Node(null, null, state, 1)
 
-
   def chess(game: State, trace: Boolean): Commands = {
     var g = game
     while (!g.terminal && g.commands.size <= g.w.w * g.w.h) {
       if (trace) println(g.w)
+      maps = g.w.remainingLambdaPositions map (p => p -> mkDistMap(g.w, p)) toMap
       val leaves = mkTree(g).leaves
       if (trace) {
         //leaves.foreach(l => println("%s: %s".format(l.commands.mkString, score(l.state))))
